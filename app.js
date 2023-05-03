3c90d45d45d5f549243bb9f931e91911d5d3b3cf
Keman Nguyen <62680940+kemannguyen@users.noreply.github.com>
2023-05-03 17:05:11 +0200

    feat: turnbased action making, affects in the same time tho.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ let numberOfPlayers = 0;
 let selectionIndex = 0;
 let myPlayerIndex;
 let myName = "";
+let yourTurn;
 
 //buttons
 const startBtn = document.getElementById("start-btn");
@@ -77,8 +78,10 @@ function selectionFunction() {
     attackBtn.style.opacity = 0.3;
     console.log("true");
   } else {
-    attackBtn.disabled = false;
-    attackBtn.style.opacity = 0.8;
+    if (yourTurn) {
+      attackBtn.disabled = false;
+      attackBtn.style.opacity = 0.8;
+    }
   }
 }
 
@@ -240,6 +243,7 @@ function numberOfPlayerFunction() {
   }
 
   function initGame() {
+    let realAction = true;
     attackBtn.disabled = true;
     attackBtn.style.opacity = 0.3;
 
@@ -251,6 +255,8 @@ function numberOfPlayerFunction() {
     nameTag.classList.add("Character");
 
     const roleText = document.createElement("div");
+    const playerTurnText = document.createElement("div");
+
     playerRef.on("value", async (snapshot) => {
       nameTag.innerHTML = `
     <div class="profile-background grid-cell-profile"></div>
@@ -277,7 +283,6 @@ function numberOfPlayerFunction() {
 
       roleText.innerHTML = `
          <span class="role-text" style="color:white"></span>
-         
       `;
       gameContainer.appendChild(roleText);
       roleText.querySelector(".role-text").innerText =
@@ -296,6 +301,25 @@ function numberOfPlayerFunction() {
       `;
     gameContainer.appendChild(confirmActionsBtn);
 
+    var playerTurnRef = firebase.database().ref(`game/playerTurn`);
+    playerTurnRef.on("value", async (snapshot) => {
+      playerTurnText.innerHTML = `
+         <span class="turn-text" style="color:white"></span>
+      `;
+      gameContainer.appendChild(playerTurnText);
+      let activePlayerIndex = await snapshot.val();
+      let activePlayerName = "";
+      Object.keys(players).forEach((key) => {
+        if (players[key].index == activePlayerIndex) {
+          activePlayerName = players[key].name;
+        }
+      });
+      console.log(activePlayerIndex);
+      if (activePlayerName != "") {
+        playerTurnText.querySelector(".turn-text").innerText =
+          activePlayerName + "'s turn";
+      }
+    });
     undoButton = document.getElementById("undo-btn");
     confirmButton = document.getElementById("confirm-btn");
 
@@ -308,6 +332,9 @@ function numberOfPlayerFunction() {
     new KeyPressListener("KeyS", () => handleArrowPress(0, 1));
     new KeyPressListener("KeyD", () => handleArrowPress(-1, 0));
     new KeyPressListener("KeyA", () => handleArrowPress(1, 0));
+
+    //GAME MANAGER
+    const gameRef = firebase.database().ref("game");
 
     //DRAGON
     const dragonRef = firebase.database().ref("dundrian");
@@ -360,8 +387,8 @@ function numberOfPlayerFunction() {
     });
 
     //runs when a change occurs in the DB
-    allPlayersRef.on("value", (snapshot) => {
-      players = snapshot.val() || {};
+    allPlayersRef.on("value", async (snapshot) => {
+      players = (await snapshot.val()) || {};
 
       Object.keys(players).forEach((key) => {
         const characterState = players[key];
@@ -376,6 +403,53 @@ function numberOfPlayerFunction() {
 
         el.style.transform = `translate3d(${left}, ${top}, 0)`;
 
+        //activates role button at gamestart
+        dragonRef.on("value", (snapshot) => {
+          if (snapshot.child("start").val()) {
+            activateRoleBtn.disabled = false;
+            activateRoleBtn.style.opacity = 0.8;
+            startBtn.disabled = true;
+            healBtn.disabled = false;
+            healBtn.style.opacity = 0.8;
+            bosshealBtn.disabled = false;
+            bosshealBtn.style.opacity = 0.8;
+            parryBtn.disabled = false;
+            parryBtn.style.opacity = 0.8;
+          } else {
+            activateRoleBtn.disabled = true;
+            activateRoleBtn.style.opacity = 0.3;
+            startBtn.disabled = false;
+            attackBtn.disabled = true;
+            attackBtn.style.opacity = 0.3;
+            healBtn.disabled = true;
+            healBtn.style.opacity = 0.3;
+            bosshealBtn.disabled = true;
+            bosshealBtn.style.opacity = 0.3;
+            parryBtn.disabled = true;
+            parryBtn.style.opacity = 0.3;
+          }
+        });
+        gameRef.on("value", (snapshot) => {
+          if (snapshot.child("playerTurn").val() != myPlayerIndex - 1) {
+            yourTurn = false;
+            attackBtn.disabled = true;
+            attackBtn.style.opacity = 0.3;
+            healBtn.disabled = true;
+            healBtn.style.opacity = 0.3;
+            bosshealBtn.disabled = true;
+            bosshealBtn.style.opacity = 0.3;
+            parryBtn.disabled = true;
+            parryBtn.style.opacity = 0.3;
+          } else {
+            yourTurn = true;
+            healBtn.disabled = false;
+            healBtn.style.opacity = 0.8;
+            bosshealBtn.disabled = false;
+            bosshealBtn.style.opacity = 0.8;
+            parryBtn.disabled = false;
+            parryBtn.style.opacity = 0.8;
+          }
+        });
         //Change character if role activated
         if (players[key].revealed) {
           if (players[key].role == "cultist") {
@@ -523,6 +597,12 @@ function numberOfPlayerFunction() {
       console.log(roleArr);
       AutoSortPlayers();
       dragonRef.update({ start: true });
+
+      var game = {
+        turn: 0,
+        playerTurn: "",
+      };
+      gameRef.set(game);
     });
 
     //Auto sort players
@@ -587,6 +667,14 @@ function numberOfPlayerFunction() {
             });
             newHp -= 5;
             selectionRef.child("/hp").set(newHp);
+            if (realAction) {
+              gameRef.child(`${playerId}`).child("action").set({ attack: key });
+            } else {
+              gameRef
+                .child(`${playerId}`)
+                .child("fakeaction")
+                .set({ attack: key });
+            }
           }
         });
       } else {
@@ -598,7 +686,20 @@ function numberOfPlayerFunction() {
         });
         newHp -= 5;
         selectionRef.child("/hp").set(newHp);
+        if (realAction) {
+          gameRef
+            .child(`${playerId}`)
+            .child("action")
+            .set({ attack: "dundrian" });
+        } else {
+          gameRef
+            .child(`${playerId}`)
+            .child("fakeaction")
+            .set({ attack: "dundrian" });
+        }
       }
+      confirmButton.hidden = false;
+      undoButton.hidden = false;
     });
     healBtn.addEventListener("click", () => {
       console.log("heal", selectionIndex);
@@ -606,16 +707,19 @@ function numberOfPlayerFunction() {
       if (selectionIndex > 0) {
         Object.keys(players).forEach((key) => {
           if (players[key].index + 1 == selectionIndex) {
-            var selectionRef = firebase.database().ref(`players/${key}`);
-            let newHp;
-            selectionRef.on("value", (snapshot) => {
-              newHp = snapshot.child("/hp").val();
-            });
-            newHp += 5;
-            selectionRef.child("/hp").set(newHp);
+            if (realAction) {
+              gameRef.child(`${playerId}`).child("action").set({ heal: key });
+            } else {
+              gameRef
+                .child(`${playerId}`)
+                .child("fakeaction")
+                .set({ heal: key });
+            }
           }
         });
       }
+      confirmButton.hidden = false;
+      undoButton.hidden = false;
     });
     bosshealBtn.addEventListener("click", () => {
       console.log("boss heal", selectionIndex);
@@ -623,27 +727,115 @@ function numberOfPlayerFunction() {
       //USE THIS AFTER ACTIONS HAS BEEN MADE
       confirmButton.hidden = false;
       undoButton.hidden = false;
-      playerRef.child("role").set("ninja");
+      if (realAction) {
+        gameRef.child(`${playerId}`).child("action").set({ heal: "dundrian" });
+      } else {
+        gameRef
+          .child(`${playerId}`)
+          .child("fakeaction")
+          .set({ heal: "dundrian" });
+      }
     });
     parryBtn.addEventListener("click", () => {
       console.log("parry", selectionIndex);
-      playerRef.child("role").set("cultist");
+      confirmButton.hidden = false;
+      undoButton.hidden = false;
+      if (realAction) {
+        gameRef.child(`${playerId}`).child("action").set({ parry: playerId });
+      } else {
+        gameRef
+          .child(`${playerId}`)
+          .child("fakeaction")
+          .set({ parry: playerId });
+      }
     });
     activateRoleBtn.addEventListener("click", () => {
       console.log("parry", selectionIndex);
       playerRef.child("revealed").set(true);
+      playerRef.once("value", (snap) => {
+        if (snap.child("role").val() == "paladin") {
+          return;
+        }
+        nameTag
+          .querySelector(".img_profile")
+          .setAttribute(
+            "src",
+            `images/dundrian-${snap.child("role").val()}.gif`
+          );
+        if (snap.child("role").val() == "cultist") {
+          nameTag
+            .querySelector(".img_profile")
+            .setAttribute("style", `margin-left: -22; margin-top: 5`);
+        } else if (snap.child("role").val() == "ninja") {
+          nameTag
+            .querySelector(".img_profile")
+            .setAttribute("style", ` margin-top: 5`);
+        }
+      });
     });
 
     confirmButton.addEventListener("click", () => {
       console.log("confirm");
+      //Next player turn
+      if (!realAction) {
+        let newPlayerIndex;
+        console.log(myPlayerIndex);
+        if (myPlayerIndex >= numberOfPlayersHook.numberOfPlayers) {
+          newPlayerIndex = 0;
+          gameRef.once("value", (snap) => {
+            gameRef.child("turn").set(snap.child("turn").val() + 1);
+          });
+          Object.keys(players).forEach((key) => {
+            var actionRef = firebase.database().ref(`game/${key}/action`);
+            actionRef.once("value", async (snap) => {
+              let string = JSON.stringify(await snap.val());
+              string = string.replace("{", "");
+              string = string.replace("}", "");
+              string = string.replaceAll('"', "");
+              let data = string.split(":");
+              let action = data[0];
+              let target = data[1];
+
+              if (action == "heal") {
+                let newHp;
+                allPlayersRef.child(target).once("value", (snap) => {
+                  newHp = snap.child("hp").val();
+                });
+                let diceThrow = parseInt(Math.random() * 6);
+                diceThrow += 1;
+                console.log(diceThrow);
+                newHp += diceThrow;
+
+                allPlayersRef.child(target).child("hp").set(newHp);
+              }
+              console.log("action", action);
+              console.log("target", target);
+            });
+          });
+        } else {
+          newPlayerIndex = myPlayerIndex;
+        }
+        gameRef.child("playerTurn").set(newPlayerIndex);
+        realAction = true;
+        confirmButton.hidden = true;
+        undoButton.hidden = true;
+      } else {
+        realAction = false;
+        confirmButton.hidden = true;
+        undoButton.hidden = true;
+      }
     });
     undoButton.addEventListener("click", () => {
       console.log("undo");
+      realAction = true;
+      gameRef.child(`${playerId}`).child("action").set({});
+      gameRef.child(`${playerId}`).child("fakeaction").set({});
     });
 
     //Stops the game when someone leaves
     window.onbeforeunload = function () {
       dragonRef2.child("/start").set(false);
+      gameRef.remove();
     };
   }
 
@@ -721,7 +913,7 @@ function numberOfPlayerFunction() {
                   x: pos.x,
                   y: pos.y,
                   index: creationIndex,
-                  role: "unknown",
+                  role: "unassigned",
                   revealed: false,
                 });
                 myName = "player " + (creationIndex + 1);
